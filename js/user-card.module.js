2ae06fed6625ab9672f8719c5c033d52107e8d1f
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-10-14 18:47:25 -0500

    Mejoras a la pantalla de usuarios

--- js/user-card.module.js
+++ js/user-card.module.js
@@ -1,52 +1,10 @@
 import {request} from './services/xmlhttp-promise.module.js';
 
 let createUserCard = function(user){
-    let div = document.createElement('div');
-    div.classList.add('user-card');
-
-    //TODO ADD USER PROFILE PICTURE
-    // NAME
-    // USER TYPE RADIO
-    // EDIT
-    // DELETE
-    
-    let u = document.createElement('h5');
-    u.classList.add('user-name');
-    u.innerText = user.name;
-
-    let email = document.createElement('h5');
-    email.classList.add('user-email');
-    email.innerText = user.email;
-
-    let userType = document.createElement('h5')
-    userType.classList.add('user-type');
-    userType.innerText = user.type;
-
-    let editBtn = document.createElement('button');
-    editBtn.innerText = "Editar";
-    editBtn.addEventListener("click",e=>{
-        e.preventDefault();
-        //mandar request get para la pagina de edicion
-        request({url:'...'})
-        .then()
-        .catch();
-    });
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.innerText = "Eliminar";
-    deleteBtn.addEventListener("click",e=>{
-        e.preventDefault();
-        //mandar request de borrar
-        request({url:'...', method:'DELETE', body:{email:user.email}})
-        .then(data=>{
-            deleteBtn.parentNode.remove();
-        })
-        .catch();
-    });
-
-    div.append(u,email,userType, editBtn, deleteBtn);
-
-    return div;
+    return "<tr> <th scope='row'>img</th><td class='user-name'>" + user.name 
+    + "</td><td class='user-email'>" + user.email 
+    +"</td><td class='user-type'>" + user.type 
+    + "</td><td><button class='btn btn-outline-danger button-delete'><i class='fas fa-user-minus'></i></button><button class='btn btn-outline-secondary button-edit'><i class='fas fa-edit'></i></button></td></tr>";
 }
 
 export{createUserCard}
--- js/user-page-mock.module.js
+++ js/user-page-mock.module.js
@@ -1,20 +1,44 @@
 import {createUserCard} from './user-card.module.js'
 import {request} from './services/xmlhttp-promise.module.js';
 
-function loadData(){
-    let sr = document.querySelector('.search-results');
-    request({url:'/mock/usuarios.json'})
-    .then(usersJSON=>{
-        let users = JSON.parse(usersJSON);
-        users.forEach(user=>{
-            sr.appendChild(createUserCard(user));
-        });
-    })
-    .catch(err=>{
-        console.error("Fallo la request de usuarios "+err)
+function loadData(sr){
+    let body = sr.find("tbody");
+
+    $.getJSON(
+        '/mock/usuarios.json',
+        function(users){
+            $.each(users, function(key, user){
+                body.append(createUserCard(user));
+            });
+        }
+    ).fail(function(err){
+        console.error("Fallo la request de usuarios " + err);
     });
 }
 
-window.onload = function(){
-    loadData()
-}
+$(document).ready(function(){
+
+    let sr = $("#result-table");
+
+    sr.on("click", ".button-delete", function (){
+        if(confirm("¿Estás seguro?")){
+            //alert("ded.");
+            $.get(
+                '...',
+                'data',
+                function(data){
+
+                }
+            ).error(function(err){
+
+            });
+        }
+    });
+
+    sr.on("click", ".button-edit", function (){
+        
+    });
+
+
+    loadData(sr);
+});
