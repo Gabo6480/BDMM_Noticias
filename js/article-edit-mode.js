97dba1d3049edfc7576f0867dbeba930f80da9cc
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-11-14 23:58:30 -0600

    Cambio de color del estilo de ContentEditable

--- js/article-edit-mode.js
+++ js/article-edit-mode.js
@@ -1,11 +1,15 @@
+//import * as parser from './article-content-parser.module.js'
+
 $(document).ready(()=>{
     $('.modo-reportero-editor').click(()=>{
         $('.editor-editable').each(function(index){
             let self = $(this);
-            if(self.attr('contentEditable') == "true")
+            if(self.attr('contentEditable') == "true"){
                 self.attr('contentEditable', "false");
-            else
+            }
+            else{
                 self.attr('contentEditable', "true");
+            }
         });
     });
 });
