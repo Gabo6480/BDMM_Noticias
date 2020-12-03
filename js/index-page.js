f7fbbdb7a26bc0434ac1f64e10a1810a3ecc8944
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-12-03 12:52:41 -0600

    getByState en index

--- js/index-page.js
+++ js/index-page.js
@@ -1,13 +1,13 @@
 import {createItemCard} from './imports/index-item-card.module.js'
 import {createCarousel} from './imports/carousel-creator.module.js'
 
-import {getAll} from './services/noticias.service.js';
+import {getByState} from './services/noticias.service.js';
 
 $(document).ready(function(){
 
     var grid = $(".grid");
 
-    getAll("publicada")
+    getByState("publicada")
     .then(res=>res.json())
     .then(noticias=>{
 
@@ -43,88 +43,5 @@ $(document).ready(function(){
         });
         
     })
-
-    
-
-   /* var card = {
-        "title": "Hola equisdedededede",
-        "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-        "url": "./pages/article.html"
-    }
-
-    grid.append(createItemCard(card, "big-card"));*/
-/*
-    //Primero agregamos el contenido
-    for(var i = 0; i < 9; i++){
-        grid.append(createItemCard(card, "small-card"));
-    }
-
-    //Lo organizamos
-    grid.masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 210
-    });
-
-    var carousel = {
-        "title":"Trending aasdasdasd",
-        "cards":[
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            },
-            {
-                "title": "Hola equisdedededede",
-                "image": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
-                "url": "./pages/article.html"
-            }
-        ]
-    };
-
-    for(var i = 0; i < 3; i++){
-        $("#carousel-holder").append(createCarousel(carousel));
-    }
-
-    //Escalamos el texto dentro de cada "noticia"
-    $(".post-title-card").children().each(function(){
-        $(this).fitText(1.25);
-    });
-
-*/
 
 });
