9116d4177438937f167f89c0524ec7b9f49c7f32
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-12-03 16:00:27 -0600

    Publicadas ineditables

--- js/imports/myposts-card.module.js
+++ js/imports/myposts-card.module.js
@@ -8,9 +8,9 @@ let createMyPostCard = function(post){
     + "</td><td class='article-section'>" +  post.Seccion
     + "</td><td class='article-state'>" + post.Estado
     + "</td><td class='actions'>"
-    + "<button class='btn btn-outline-primary button-edit'><i class='fas fa-edit'></i></button>"
-    + "<button class='btn btn-outline-success button-send' disabled><i class='fas fa-arrow-up'></i></button>"
-    + "<button class='btn btn-outline-danger button-delete'><i class='far fa-trash-alt'></i></button>"
+    + (post.Estado == "publicada" ? "" : "<button class='btn btn-outline-primary button-edit'><i class='fas fa-edit'></i></button>")
+    + (post.Estado == "en redaccion" ? "<button class='btn btn-outline-success button-send' disabled><i class='fas fa-arrow-up'></i></button>" : "")
+    + (post.Estado == "publicada" ? "" : "<button class='btn btn-outline-danger button-delete'><i class='far fa-trash-alt'></i></button>")
     + "</td></tr>";
 }
 
